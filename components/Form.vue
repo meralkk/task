d70b18e372c26d5de97a0926e684f5541190f770
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-02-23 20:58:48 +0100

    created inputs

---
+++ components/Form.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="form-container">
+    <input type="text" placeholder="Service Name" class="form-control mb-2">
+    <input type="text" placeholder="Business Name" class="form-control mb-2">
+    <input type="text" placeholder="Service Rating" class="form-control mb-2">
+    <button type="button" class="btn btn-primary">Submit</button>
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+.form-container {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    margin-top: 100px;
+    gap: 20px;
+    width: 100%; 
+    max-width: 500px; 
+  }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Form from "../components/Form.vue"
+</script>
+
+<template>
+    <div>
+<Form></Form>
+    </div>
+</template>
